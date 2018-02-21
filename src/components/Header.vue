206f9022ab13b93dfd85ea057c0c0b547623c6dd
rinaldydwii <rinaldydwii@gmail.com>
2018-02-21 19:59:20 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="title">
+        Curriculum Vitae
+      </div>
+      <div class="name">
+        <h1>Rinaldy Dwi Istanto</h1>
+      </div>
+      <div class="job-position">
+        <h2>Front End Developer</h2>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  name: 'HomePage',
+  components: {
+    Header
+  }
+}
+</script>
